16ec7398c13d5a2c42a226ea813e61c48bafe666
Henry Jin <henry@entrancesoftware.com>
2015-09-21 09:15:17 -0500

    Add not found route and page

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,12 @@ var React = require('react');
 var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
 
 var routes = (
   <Route name="app" path="/" handler={require('./components/app')}>
     <DefaultRoute handler={require('./components/homePage')} />
+    <NotFoundRoute handler={require('./components/NotFoundPage')} />
     <Route name="authors" handler={require('./components/authors/authorPage')} />
     <Route name="about" handler={require('./components/about/aboutPage')} />
   </Route>
