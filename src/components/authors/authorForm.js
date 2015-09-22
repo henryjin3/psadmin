b212be75875d5fea7d276b13f001dbcf2a1d795a
Henry Jin <henry@entrancesoftware.com>
2015-09-22 09:13:13 -0500

    Add reusable text input component.

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -1,31 +1,26 @@
 "use strict";
 
 var React = require('react');
+var TextInput = require('../common/textInput');
 
 var AuthorForm = React.createClass({
   render: function() {
     return (
       <form>
         <h1>Manage Author</h1>
-        <label htmlFor="firstName">First Name</label>
-        <input type="text"
-          name="firstName"
-          className="form-control"
-          placeholder="First Name"
-          ref="firstName"
-          onChange={this.props.onChange}
-          value={this.props.author.firstName} />
-        <br />
+          <TextInput
+            name="firstName"
+            label="First Name"
+            value={this.props.author.firstName}
+            onChange={this.props.onChange} />
+          <br />
 
-        <label htmlFor="lastName">Last Name</label>
-        <input type="text"
-          name="lastName"
-          className="form-control"
-          placeholder="Last Name"
-          ref="lastName"
-          onChange={this.props.onChange}
-          value={this.props.author.lastName} />
-        <br />
+          <TextInput
+            name="lastName"
+            label="Last Name"
+            value={this.props.author.lastName}
+            onChange={this.props.onChange} />
+          <br />
 
         <input type="submit" value="Save" className="btn btn-default" />
       </form>
