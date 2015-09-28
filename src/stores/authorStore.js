20a0c4cba6caf88dd066552802152dde2d0b1057
Henry Jin <henry@entrancesoftware.com>
2015-09-28 13:53:18 -0500

    Add core author store functions

---
+++ src/stores/authorStore.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var ActionTypes = require('../constants/actionTypes');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+var CHANGE_EVENT = 'change';
+
+// what's different about this and extending?
+var AuthorStore = assign({}, EventEmitter.prototype, {
+  addChangeListener: function(callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+
+  removeChangeListener: function(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  },
+
+  emitChange: function() {
+    this.emit(CHANGE_EVENT);
+  }
+});
+
+module.exports = AuthorStore;
